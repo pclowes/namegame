707904a3d2ca69be222b57a76510a1ba2590e3c7
pclowes <peter.d.clowes@gmail.com>
2015-01-29 20:54:21 -0700

    started creation of index method

--- app/controllers/subimages_controller.rb
+++ app/controllers/subimages_controller.rb
@@ -1,8 +1,15 @@
 class SubimagesController < ApplicationController
+  require 'base64'
+
   def create
     # subimage_params = params.require(:subimage).permit(:subimage, :name)
+
     @subimage = Subimage.new(subimage: params[:subimage])
     @subimage.save
     redirect_to root_path
+  end
+
+  def index
+    @theimage = Subimage.find(1)
   end
 end
