3560f35d7aee208d9d00ac7ebc7829499c51355c
pclowes <peter.d.clowes@gmail.com>
2015-02-18 12:56:04 -0700

    added webcam functionality

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['S3_KEY'],
-    aws_secret_access_key: ENV['S3_SECRET'],
-  }
+  # config.fog_credentials = {
+  #   provider: 'AWS',
+  #   aws_access_key_id: ENV['S3_KEY'],
+  #   aws_secret_access_key: ENV['S3_SECRET'],
+  # }
 
   #for testing, upload files to local 'tmp' folder.
   if Rails.env.test? || Rails.env.cucumber?
